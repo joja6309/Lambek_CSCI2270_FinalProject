9c845f999cd44403a11ce72938af0e518ed1785b
joja6309 <joseph.jackson@colorado.edu>
2015-05-02 11:12:54 -0600

    Update main.cpp
    Added a if/else and print to show the user if the number that  they entered is binary

--- main.cpp
+++ main.cpp
@@ -10,7 +10,14 @@ int primeMax()
     string prime;
     cout<<"Find all primes up to what number?"<<endl;
     cin>>prime;
-
+   if ( binaryCheck(prime)) 
+   { 
+       "Hey this number is binary!"
+       }
+       else 
+      { 
+          "This number is not binary"
+        };
     return atoi(prime.c_str());
 }
 
@@ -18,5 +25,9 @@ int primeMax()
 int main()
 {
     primeSieve pS(primeMax());
+    
+  
+
+    
     return 0;
 }
